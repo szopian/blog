22bcfd4119ee4140f62374cbcfb86cf209bee6ac
Adri Ana <szopiangreg@gmail.com>
2021-04-03 23:27:57 +0100

    add

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("greg");
+  const [age, setAge] = useState("32");
+
   const handleClick = () => {
-    console.log("hey ninjas");
+    setName("adri");
+    setAge("38");
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me</button>
+      <p>{name + age}</p>
     </div>
   );
 };

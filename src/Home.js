f335d383ea636d774c7c1979ecc9d21cdd2114d4
Adri Ana <szopiangreg@gmail.com>
2021-04-04 20:38:32 +0100

    add

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: "blog1", body: "blog text...", author: "x1", id: 1 },
-    { title: "blog2", body: "blog text...", author: "x2", id: 2 },
-    { title: "blog3", body: "blog text...", author: "x1", id: 3 },
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
   ]);
+
+  const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -14,12 +21,14 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log("used effect");
-  });
+    console.log("use effect ran");
+    console.log(blogs);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
     </div>
   );
 };

bfb5462d1898c27aae0417033a4a384d97020849
Adri Ana <szopiangreg@gmail.com>
2021-04-04 14:52:46 +0100

    add author blog list

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,16 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "blog1", body: "blog text...", author: "x1", id: 1 },
     { title: "blog2", body: "blog text...", author: "x2", id: 2 },
-    { title: "blog3", body: "blog text...", author: "x3", id: 3 },
+    { title: "blog3", body: "blog text...", author: "x1", id: 3 },
   ]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "x1")}
+        title="x1 blogs"
+      />
     </div>
   );
 };

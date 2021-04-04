b9c9589de9ec31071300f0b9f9e8a3d6ab2103ef
Adri Ana <szopiangreg@gmail.com>
2021-04-04 20:46:47 +0100

    add database json file

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
+    { title: "blog1", body: "blog text...", author: "x1", id: 1 },
+    { title: "blog2", body: "blog text...", author: "x2", id: 2 },
+    { title: "blog3", body: "blog text...", author: "x1", id: 3 },
   ]);
 
-  const [name, setName] = useState("mario");
+  const [name, setName] = useState("person1");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -21,14 +16,15 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(blogs);
+    console.log("used effect");
+    console.log(name);
   }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("luigi")}>change name</button>
+      <button onClick={() => setName("Greg")}>change me</button>
+      <p>{name}</p>
     </div>
   );
 };

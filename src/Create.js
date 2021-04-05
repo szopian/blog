6c1f650c4517043de51dcc8c75ea145ec63c4ff6
Adri Ana <szopiangreg@gmail.com>
2021-04-05 17:23:04 +0100

    add submit to create page

--- src/Create.js
+++ src/Create.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("x1");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form>
         <label>Blog title</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select>
-          <option value=""></option>
-          <option value=""></option>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="x1">x1</option>
+          <option value="x2">x2</option>
         </select>
         <button>Add blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
